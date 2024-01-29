570de5110347b2fd0b20953b35da33a30918cb74
vinith0309_ec0429430225 <vinith0309@gmail.com>
2024-01-29 16:40:56 +0530

    after shadcn confused

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
+import { TaskStatus } from "@prisma/client";
+import { RouterOutputs, api } from "@/utils/api";
+import { Button } from "@/components/ui/button";
 
-import { api } from "@/utils/api";
+type Task = RouterOutputs["tasks"]["getAll"][number];
+interface KanbanCard {
+  name: string;
+  color: string;
+  tasks: Task[];
+}
 
 export default function Home() {
   const { data } = api.tasks.getAll.useQuery();
-  console.log(data);
+
   return (
     <>
       <Head>
@@ -19,8 +26,66 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gray-200">
         <AuthShowcase />
+        <Button>+ New Task</Button>
+        <Kanban tasks={data ?? []} />
       </main>
     </>
+  );
+}
+
+const statusOptions = [
+  { type: TaskStatus.BACKLOG, color: "bg-card-blue" },
+  { type: TaskStatus.IN_PROGRESS, color: "bg-card-yellow" },
+  { type: TaskStatus.WAITING_FOR_REVIEW, color: "bg-card-blue-2" },
+  { type: TaskStatus.DONE, color: "bg-card-green" },
+  { type: TaskStatus.STUCK, color: "bg-card-red" },
+];
+
+function TaskCard(props: Task) {
+  return (
+    <div className="mt-2 overflow-hidden rounded-lg border bg-white p-3 hover:shadow-md">
+      <div className="flex justify-between p-2 font-bold capitalize text-black">
+        {props.name}
+      </div>
+      <div className=" flex">{props.priority}</div>
+    </div>
+  );
+}
+
+function KanbarCard({ name, color, tasks = [] }: KanbanCard) {
+  return (
+    <div
+      className={`bg-card-grey hover:bg-card-grey-2 w-64 overflow-hidden rounded-lg hover:shadow-md`}
+    >
+      <div className={`${color} z-10 p-2 font-bold capitalize text-white`}>
+        {name.split("_").join(" ")} / {tasks?.length ?? 0}
+      </div>
+      <div className="min-h-60 px-3 pb-4 pt-1">
+        {tasks?.map((task) => {
+          return <TaskCard key={task.id} {...task} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+function Kanban({ tasks }: { tasks: Task[] }) {
+  return (
+    <div className="flex gap-2">
+      {statusOptions.map(({ type, color }) => {
+        const groupedTasks: Task[] = tasks?.filter(
+          (task: Task) => task.status === type,
+        );
+        return (
+          <KanbarCard
+            key={type}
+            name={type}
+            color={color}
+            tasks={groupedTasks}
+          />
+        );
+      })}
+    </div>
   );
 }
 
--- src/server/api/routers/task.ts
+++ src/server/api/routers/task.ts
@@ -10,4 +10,14 @@ export const taskRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.task.findMany();
   }),
+  // create: publicProcedure.mutation(({ ctx, input }) => {
+  //   return ctx.db.task.create({
+  //     data: {
+  //       title: input.title,
+  //       description: input.description,
+  //       status: input.status,
+  //       userId: input.userId,
+  //     },
+  //   });
+  // }),
 });
